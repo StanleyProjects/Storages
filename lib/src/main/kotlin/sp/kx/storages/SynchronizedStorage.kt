9a8ac1f0045c93ff700e5682f4c52b93442a9a58
Stanley Wintergreen <kepocnhh@gmail.com>
2024-04-30 02:59:27 +0300

    fix synchronized

--- lib/src/main/kotlin/sp/kx/storages/SynchronizedStorage.kt
+++ lib/src/main/kotlin/sp/kx/storages/SynchronizedStorage.kt
@@ -2,5 +2,6 @@ package sp.kx.storages
 
 interface SynchronizedStorage<T : Any> : MutableStorage<T> {
     fun merge(info: MergeInfo): List<Described<ByteArray>>
+    fun getSyncInfo(): SyncInfo
     fun getMergeInfo(info: SyncInfo): MergeInfo
 }
