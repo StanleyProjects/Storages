9f4cae54b3a10b95333a1e613fe229c4bf3ea6aa
Stanley Wintergreen <kepocnhh@gmail.com>
2024-04-30 03:07:52 +0300

    snapshot...

--- README.md
+++ README.md
@@ -3,6 +3,31 @@ A library for storing, changing and synchronizing data between devices.
 
 ---
 
+## Snapshot
+
+![version](https://img.shields.io/static/v1?label=version&message=0.1.0-SNAPSHOT&labelColor=212121&color=2962ff&style=flat)
+
+- [Maven](https://s01.oss.sonatype.org/content/repositories/snapshots/com/github/kepocnhh/Storages/0.1.0-SNAPSHOT)
+- [Documentation](https://StanleyProjects.github.io/Storages/doc/0.1.0-SNAPSHOT)
+
+### Build
+```
+$ gradle lib:assembleSnapshotJar
+```
+
+### Import
+```kotlin
+repositories {
+    maven("https://s01.oss.sonatype.org/content/repositories/snapshots")
+}
+
+dependencies {
+    implementation("com.github.kepocnhh:Storages:0.1.0-SNAPSHOT")
+}
+```
+
+---
+
 ## Unstable
 
 ![version](https://img.shields.io/static/v1?label=version&message=0.0.1u-SNAPSHOT&labelColor=212121&color=2962ff&style=flat)
--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -1,5 +1,7 @@
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import sp.gx.core.Badge
+import sp.gx.core.GitHub
 import sp.gx.core.Markdown
 import sp.gx.core.Maven
 import sp.gx.core.asFile
@@ -7,6 +9,10 @@ import sp.gx.core.assemble
 import sp.gx.core.buildDir
 import sp.gx.core.check
 import sp.gx.core.create
+import sp.gx.core.existing
+import sp.gx.core.file
+import sp.gx.core.filled
+import sp.gx.core.resolve
 import sp.gx.core.task
 
 version = "0.1.0"
@@ -16,10 +22,16 @@ val maven = Maven.Artifact(
     id = rootProject.name,
 )
 
+val gh = GitHub.Repository(
+    owner = "StanleyProjects",
+    name = rootProject.name,
+)
+
 repositories.mavenCentral()
 
 plugins {
     id("org.jetbrains.kotlin.jvm")
+    id("org.jetbrains.dokka") version Version.dokka
 }
 
 val compileKotlinTask = tasks.getByName<KotlinCompile>("compileKotlin") {
@@ -106,6 +118,113 @@ dependencies {
                     ),
                 )
             println("POM: ${file.absolutePath}")
+        }
+    }
+}
+
+"snapshot".also { variant ->
+    val version = "$version-SNAPSHOT"
+    tasks.create("check", variant, "Readme") {
+        doLast {
+            val badge = Markdown.image(
+                text = "version",
+                url = Badge.url(
+                    label = "version",
+                    message = version,
+                    color = "2962ff",
+                ),
+            )
+            val expected = setOf(
+                badge,
+                Markdown.link("Maven", Maven.Snapshot.url(maven, version)),
+                "implementation(\"${maven.moduleName(version)}\")",
+            )
+            rootDir.resolve("README.md").check(
+                expected = expected,
+                report = buildDir()
+                    .dir("reports/analysis/readme")
+                    .asFile("index.html"),
+            )
+        }
+    }
+    tasks.create("assemble", variant, "MavenMetadata") {
+        doLast {
+            val file = buildDir()
+                .dir("yml")
+                .file("maven-metadata.yml")
+                .assemble(
+                    """
+                        repository:
+                         groupId: '${maven.group}'
+                         artifactId: '${maven.id}'
+                        version: '$version'
+                    """.trimIndent(),
+                )
+            println("Metadata: ${file.absolutePath}")
+        }
+    }
+    task<Jar>("assemble", variant, "Jar") {
+        dependsOn(compileKotlinTask)
+        archiveBaseName = maven.id
+        archiveVersion = version
+        from(compileKotlinTask.destinationDirectory.asFileTree)
+    }
+    task<Jar>("assemble", variant, "Source") {
+        archiveBaseName = maven.id
+        archiveVersion = version
+        archiveClassifier = "sources"
+        from(sourceSets.main.get().allSource)
+    }
+    tasks.create("assemble", variant, "Pom") {
+        doLast {
+            val file = buildDir()
+                .dir("libs")
+                .file("${maven.name(version)}.pom")
+                .assemble(
+                    maven.pom(
+                        version = version,
+                        packaging = "jar",
+                    ),
+                )
+            println("POM: ${file.absolutePath}")
+        }
+    }
+    task<DokkaTask>("assemble", variant, "Documentation") {
+        outputDirectory = layout.buildDirectory.dir("documentation/$variant")
+        moduleName = gh.name
+        moduleVersion = version
+        dokkaSourceSets.getByName("main") {
+            val path = "src/$name/kotlin"
+            reportUndocumented = false
+            sourceLink {
+                localDirectory = file(path)
+                remoteUrl = gh.url().resolve("tree", moduleVersion.get(), "lib", path)
+            }
+            jdkVersion = Version.jvmTarget.toInt()
+        }
+        doLast {
+            val index = outputDirectory.get()
+                .file("index.html")
+                .existing()
+                .file()
+                .filled()
+            println("Documentation: ${index.absolutePath}")
+        }
+    }
+    tasks.create("assemble", variant, "Metadata") {
+        doLast {
+            val file = buildDir()
+                .dir("yml")
+                .file("metadata.yml")
+                .assemble(
+                    """
+                        repository:
+                         owner: '${gh.owner}'
+                         name: '${gh.name}'
+                        version: '$version'
+                    """.trimIndent(),
+                )
+            println("Metadata: ${file.absolutePath}")
         }
     }
 }
