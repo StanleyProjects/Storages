d0fc25df2e1e7750ab0cf03178d6f409917cc5c3
Stanley Wintergreen <kepocnhh@gmail.com>
2024-05-05 23:22:33 +0300

    doc SyncInfo

--- lib/src/main/kotlin/sp/kx/storages/SyncInfo.kt
+++ lib/src/main/kotlin/sp/kx/storages/SyncInfo.kt
@@ -2,6 +2,17 @@ package sp.kx.storages
 
 import java.util.UUID
 
+/**
+ * Information that can be used to compare data in another storage.
+ *
+ * Usage:
+ * ```
+ * val storage: SyncStorage<Foo> = ...
+ * val syncInfo = storage.getSyncInfo()
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.1
+ */
 data class SyncInfo(
     val meta: Map<UUID, ItemInfo>,
     val deleted: Set<UUID>,
