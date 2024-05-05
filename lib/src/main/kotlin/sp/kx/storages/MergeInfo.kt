ec6952ae44abd1bd7654e6a96ee806d9e334f193
Stanley Wintergreen <kepocnhh@gmail.com>
2024-05-05 23:21:07 +0300

    doc MergeInfo

--- lib/src/main/kotlin/sp/kx/storages/MergeInfo.kt
+++ lib/src/main/kotlin/sp/kx/storages/MergeInfo.kt
@@ -2,6 +2,19 @@ package sp.kx.storages
 
 import java.util.UUID
 
+/**
+ * Information obtained as a result of comparing data from the receiving storage to the transmitting storage.
+ *
+ * Usage:
+ * ```
+ * val receiver: SyncStorage<Foo> = ...
+ * val transmitter: SyncStorage<Foo> = ...
+ * val syncInfo = receiver.getSyncInfo()
+ * val mergeInfo = transmitter.getMergeInfo(syncInfo)
+ * ```
+ * @author [Stanley Wintergreen](https://github.com/kepocnhh)
+ * @since 0.4.1
+ */
 data class MergeInfo(
     val download: Set<UUID>,
     val items: List<Described<ByteArray>>,
