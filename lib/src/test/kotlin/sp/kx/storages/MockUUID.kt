f863c3f742dc401458a8c1b2580846a64aaa670f
Stanley Wintergreen <kepocnhh@gmail.com>
2024-05-09 15:10:56 +0300

    fix unit test

--- lib/src/test/kotlin/sp/kx/storages/MockUUID.kt
+++ lib/src/test/kotlin/sp/kx/storages/MockUUID.kt
@@ -3,5 +3,5 @@ package sp.kx.storages
 import java.util.UUID
 
 internal fun mockUUID(pointer: Int = 0): UUID {
-    return UUID.fromString("cbae2ba0-6be9-40f5-b565-d6152a1${10_000 + pointer % 1024}")
+    return UUID.fromString("cbae2ba0-6be9-40f5-b565-d6152a1${20_000 + pointer % 1024}")
 }
