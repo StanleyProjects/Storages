db333e0e52a89c0e887f692761c9a58e4499d284
Stanley Wintergreen <kepocnhh@gmail.com>
2024-07-18 18:43:07 +0300

    test hex

--- lib/src/main/kotlin/sp/kx/storages/StringUtil.kt
+++ lib/src/main/kotlin/sp/kx/storages/StringUtil.kt
@@ -1,9 +1,9 @@
 package sp.kx.storages
 
-internal fun Int.toHEX(): String {
-    return String.format("%02x", this and 0xff)
+internal fun Byte.toHEX(): String {
+    return String.format("%02x", toInt().and(0xff))
 }
 
 internal fun ByteArray.toHEX(): String {
-    return joinToString(separator = "") { it.toInt().toHEX() }
+    return joinToString(separator = "") { it.toHEX() }
 }
