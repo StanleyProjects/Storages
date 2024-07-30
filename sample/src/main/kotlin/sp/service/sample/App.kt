414b97277b92e195ab3a303a28fd9f456d4e3570
Stanley Wintergreen <kepocnhh@gmail.com>
2024-07-30 13:45:56 +0300

    file streamer

--- sample/src/main/kotlin/sp/service/sample/App.kt
+++ sample/src/main/kotlin/sp/service/sample/App.kt
@@ -1,17 +1,13 @@
 package sp.service.sample
 
 import sp.kx.storages.HashFunction
-import sp.kx.storages.Streamer
 import sp.kx.storages.SyncStreamsStorage
 import sp.kx.storages.SyncStreamsStorages
 import sp.kx.storages.Transformer
 import sp.kx.storages.require
 import java.io.File
-import java.io.InputStream
-import java.io.OutputStream
 import java.util.Locale
 import java.util.UUID
-import kotlin.math.absoluteValue
 import kotlin.time.Duration
 import kotlin.time.Duration.Companion.milliseconds
 
@@ -37,81 +33,6 @@ private object DefaultEnvironment : SyncStreamsStorage.Environment {
 
     override fun randomUUID(): UUID {
         return UUID.fromString("00000000-0000-0000-0000-0000000${10_000 + pointer++ % 1024}")
-    }
-}
-
-private class FileStreamerProvider(
-    private val dir: File,
-    ids: Set<UUID>,
-) : SyncStreamsStorages.StreamerProvider {
-
-    init {
-        File(dir, "storages").mkdirs()
-        val pointers = File(dir, "pointers")
-        if (!pointers.exists() || pointers.length() == 0L) {
-            val text = ids.joinToString(separator = ",") { id ->
-                "$id:0"
-            }
-            File(dir, "pointers").writeText(text)
-        }
-    }
-
-    override fun getStreamer(id: UUID, inputPointer: Int, outputPointer: Int): Streamer {
-        return FileStreamer(
-            dir = File(dir, "storages"),
-            id = id,
-            inputPointer = inputPointer,
-            outputPointer = outputPointer,
-        )
-    }
-
-    private fun getValues(): Map<UUID, Int> {
-        val text = File(dir, "pointers").readText()
-        check(text.isNotEmpty())
-        return text.split(",").associate {
-            val (_id, _pointer) = it.split(":")
-            UUID.fromString(_id) to _pointer.toInt()
-        }
-    }
-
-    override fun getPointer(id: UUID): Int {
-        return getValues()[id] ?: error("No pointer by ID: \"$id\"!")
-    }
-
-    override fun putPointers(values: Map<UUID, Int>) {
-        val newValues = getValues() + values
-        val text = newValues.entries.joinToString(separator = ",") { (id, pointer) ->
-            "$id:$pointer"
-        }
-        File(dir, "pointers").writeText(text)
-        for (file in File(dir, "storages").listFiles()!!) {
-            if (file.isDirectory) continue
-            val exists = newValues.any { (id, pointer) -> file.name == "$id-$pointer"}
-            if (!exists) file.delete()
-        }
-    }
-}
-
-private class FileStreamer(
-    private val dir: File,
-    private val id: UUID,
-    private val inputPointer: Int,
-    private val outputPointer: Int,
-) : Streamer {
-    override fun inputStream(): InputStream {
-        check(dir.exists())
-        check(dir.isDirectory)
-        val file = File(dir, "$id-$inputPointer")
-        if (!file.exists() || file.length() == 0L) {
-            file.writeBytes(ByteArray(12))
-        }
-        return file.inputStream()
-    }
-
-    override fun outputStream(): OutputStream {
-        check(dir.exists())
-        check(dir.isDirectory)
-        return File(dir, "$id-$outputPointer").outputStream()
     }
 }
 
@@ -214,12 +135,7 @@ fun main() {
         .build(
             hf = hf,
             env = DefaultEnvironment,
-            getStreamerProvider = { ids ->
-                FileStreamerProvider(
-                    dir = File("/tmp/sample/t"),
-                    ids = ids,
-                )
-            },
+            dir = File("/tmp/sample/t"),
         )
     tStorages.println()
     Foo(text = "foo:${nextInt()}").also { item ->
@@ -237,12 +153,7 @@ fun main() {
         .build(
             hf = hf,
             env = DefaultEnvironment,
-            getStreamerProvider = { ids ->
-                FileStreamerProvider(
-                    dir = File("/tmp/sample/r"),
-                    ids = ids,
-                )
-            },
+            dir = File("/tmp/sample/r"),
         )
     rStorages.println()
     Foo(text = "foo:${nextInt()}").also { item ->
