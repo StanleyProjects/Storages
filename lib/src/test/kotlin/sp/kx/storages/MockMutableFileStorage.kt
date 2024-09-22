59b5e2c9e64e67502c1f3d21a399e0342ccbb6e0
Stanley Wintergreen <kepocnhh@gmail.com>
2024-09-22 14:45:58 +0300

    file storage mut

--- lib/src/test/kotlin/sp/kx/storages/MockMutableFileStorage.kt
+++ lib/src/test/kotlin/sp/kx/storages/MockMutableFileStorage.kt
@@ -2,10 +2,14 @@ package sp.kx.storages
 
 import sp.kx.bytes.toByteArray
 import java.util.UUID
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.milliseconds
 
 internal class MockMutableFileStorage(
     override val id: UUID = mockUUID(),
     private val hf: HashFunction = MockHashFunction(),
+    private val uuidProvider: MockProvider<UUID> = MockProvider { mockUUID(1) },
+    private val timeProvider: MockProvider<Duration> = MockProvider { 1.milliseconds },
     values: Map<Raw, ByteArray> = emptyMap(),
 ) : MutableFileStorage {
     private val values: MutableMap<Raw, ByteArray> = values.toMutableMap()
@@ -17,11 +21,24 @@ internal class MockMutableFileStorage(
     }
 
     override fun add(bytes: ByteArray): Raw {
-        TODO("add")
+        val raw = Raw(
+            id = uuidProvider.provide(),
+            info = mockItemInfo(
+                created = timeProvider.provide(),
+                updated = timeProvider.provide(),
+                hash = hf.map(bytes = bytes),
+            ),
+        )
+        values[raw] = bytes
+        return raw
     }
 
     override fun update(id: UUID, bytes: ByteArray): ItemInfo? {
-        TODO("update")
+        val oldRaw = values.entries.firstOrNull { (raw, _) -> raw.id == id }?.key ?: return null
+        check(values.remove(oldRaw) != null)
+        val newRaw = oldRaw.copy(updated = timeProvider.provide(), hash = hf.map(bytes))
+        values[newRaw] = bytes
+        return newRaw.info
     }
 
     override val hash: ByteArray
@@ -36,6 +53,6 @@ internal class MockMutableFileStorage(
         }
 
     override fun getBytes(id: UUID): ByteArray {
-        return values.entries.firstOrNull { (raw, _) -> raw.id == id }?.value ?: TODO()
+        return values.entries.firstOrNull { (raw, _) -> raw.id == id }?.value ?: TODO("MockMutableFileStorage:getBytes($id)")
     }
 }
--- lib/src/test/kotlin/sp/kx/storages/MutableFileStorageTest.kt
+++ lib/src/test/kotlin/sp/kx/storages/MutableFileStorageTest.kt
@@ -2,8 +2,11 @@ package sp.kx.storages
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import sp.kx.bytes.toHEX
+import java.util.UUID
 
 internal class MutableFileStorageTest {
     @Test
@@ -18,5 +21,37 @@ internal class MutableFileStorageTest {
         assertEquals(storage.items.sortedBy { it.id }, listOf(Raw(id = mockUUID(1), info = mockItemInfo(1))))
         assertTrue(storage.delete(mockUUID(1)))
         assertEquals(0, storage.items.sortedBy { it.id }.size)
+    }
+
+    @Test
+    fun addTest() {
+        val raw1 = Raw(id = mockUUID(1), info = mockItemInfo(1))
+        var uuid: UUID = mockUUID(1)
+        val storage: MutableFileStorage = MockMutableFileStorage(
+            values = mapOf(raw1 to mockByteArray(1)),
+            uuidProvider = {uuid},
+        )
+        assertEquals(storage.items.sortedBy { it.id }, listOf(raw1))
+        uuid = mockUUID(2)
+        val raw2 = storage.add(mockByteArray(2))
+        assertEquals(storage.items.sortedBy { it.id }, listOf(raw1, raw2))
+        val actual = storage.getBytes(id = raw2.id)
+        val expected = mockByteArray(2)
+        assertTrue(actual.contentEquals(expected), "a: ${actual.toHEX()}, e: ${expected.toHEX()}")
+    }
+
+    @Test
+    fun updateTest() {
+        val raw1 = Raw(id = mockUUID(1), info = mockItemInfo(1))
+        val storage: MutableFileStorage = MockMutableFileStorage(
+            values = mapOf(raw1 to mockByteArray(1)),
+        )
+        assertEquals(storage.items.sortedBy { it.id }, listOf(raw1))
+        assertTrue(mockByteArray(1).contentEquals(storage.getBytes(id = raw1.id)))
+        assertNull(storage.update(mockUUID(2), mockByteArray(2)))
+        val info = storage.update(raw1.id, mockByteArray(2))
+        checkNotNull(info)
+        assertEquals(storage.items.sortedBy { it.id }, listOf(Raw(id = mockUUID(1), info = info)))
+        assertTrue(mockByteArray(2).contentEquals(storage.getBytes(id = raw1.id)))
     }
 }
