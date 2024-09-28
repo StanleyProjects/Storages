02b8cc99615b55480563015e24859dfcc6b7a062
Stanley Wintergreen <kepocnhh@gmail.com>
2024-09-28 14:18:40 +0300

    delete file storage

--- lib/src/main/kotlin/sp/kx/storages/MutableFileStorage.kt
+++
@@ -1,9 +0,0 @@
-package sp.kx.storages
-
-import java.util.UUID
-
-interface MutableFileStorage : FileStorage {
-    fun delete(id: UUID): Boolean
-    fun add(bytes: ByteArray): Metadata
-    fun update(id: UUID, bytes: ByteArray): ItemInfo?
-}
--- lib/src/main/kotlin/sp/kx/storages/SyncFileStorage.kt
+++
@@ -1,5 +0,0 @@
-package sp.kx.storages
-
-interface SyncFileStorage : MutableFileStorage {
-    fun getSyncInfo(): SyncInfo
-}
