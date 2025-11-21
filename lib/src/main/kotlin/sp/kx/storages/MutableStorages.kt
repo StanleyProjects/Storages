2922a273f7a428e23b639fffbf15f7d7c2e89356
Stanley Wintergreen <kepocnhh@gmail.com>
2025-11-21 17:42:48 +0300

    wip

--- lib/src/main/kotlin/sp/kx/storages/MutableStorages.kt
+++ lib/src/main/kotlin/sp/kx/storages/MutableStorages.kt
@@ -1,9 +1,12 @@
 package sp.kx.storages
 
+import java.util.UUID
+
 interface MutableStorages : Storages {
     class Transaction private constructor(val operations: List<Operation>) {
         sealed interface Operation {
             class Add<T : Any>(val key: Storage.Key<T>, val value: T) : Operation
+            class Delete<T : Any>(val key: Storage.Key<T>, val id: UUID) : Operation
         }
 
         class Builder {
@@ -11,6 +14,11 @@ interface MutableStorages : Storages {
 
             fun <T : Any> add(key: Storage.Key<T>, value: T): Builder {
                 operations.add(Operation.Add(key = key, value = value))
+                return this
+            }
+
+            fun <T : Any> delete(key: Storage.Key<T>, id: UUID): Builder {
+                operations.add(Operation.Delete(key = key, id = id))
                 return this
             }
 
