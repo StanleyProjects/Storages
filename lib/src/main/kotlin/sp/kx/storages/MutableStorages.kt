855591a13c97580969b96f0be76bfd03027f6610
Stanley Wintergreen <kepocnhh@gmail.com>
2025-11-21 16:56:51 +0300

    transactions

--- lib/src/main/kotlin/sp/kx/storages/MutableStorages.kt
+++ lib/src/main/kotlin/sp/kx/storages/MutableStorages.kt
@@ -1,5 +1,20 @@
 package sp.kx.storages
 
 interface MutableStorages : Storages {
+    class Transaction {
+        internal sealed interface Operation {
+            class Add<T : Any>(val key: Storage.Key<T>, value: T) : Operation
+        }
+
+        private val operations = mutableListOf<Operation>()
+
+        fun <T : Any> add(key: Storage.Key<T>, value: T): Transaction {
+            operations.add(Operation.Add(key = key, value = value))
+            return this
+        }
+    }
+
     override operator fun <T : Any> get(key: Storage.Key<T>): MutableStorage<T>?
+
+    fun commit(transaction: Transaction)
 }
