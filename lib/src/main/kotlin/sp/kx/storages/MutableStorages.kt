0a2cc5cc2ea62f12448fec0e042c88edf5d77d0c
Stanley Wintergreen <kepocnhh@gmail.com>
2025-11-21 16:41:16 +0300

    keys

--- lib/src/main/kotlin/sp/kx/storages/MutableStorages.kt
+++ lib/src/main/kotlin/sp/kx/storages/MutableStorages.kt
@@ -1,5 +1,5 @@
 package sp.kx.storages
 
 interface MutableStorages : Storages {
-    override operator fun <T : Any> get(type: Class<T>): MutableStorage<T>?
+    override operator fun <T : Any> get(key: Storage.Key<T>): MutableStorage<T>?
 }
--- lib/src/main/kotlin/sp/kx/storages/Storage.kt
+++ lib/src/main/kotlin/sp/kx/storages/Storage.kt
@@ -3,7 +3,9 @@ package sp.kx.storages
 import java.util.UUID
 
 interface Storage<T : Any> {
-    val id: UUID
+    data class Key<T : Any>(val id: UUID, val type: Class<T>)
+
+    val key: Key<T>
     val payloads: List<Payload<T>>
 
     operator fun get(id: UUID): Payload<T>?
