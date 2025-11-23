23d487c90f68cb5c7efd8abae257460226745e11
Stanley Wintergreen <kepocnhh@gmail.com>
2025-11-23 16:52:31 +0300

    0.14.2

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -15,7 +15,7 @@ import sp.kx.gradlex.dir
 import sp.kx.gradlex.eff
 import sp.kx.gradlex.get
 
-version = "0.14.1"
+version = "0.14.2"
 
 val maven = Maven.Artifact(
     group = "com.github.kepocnhh",
